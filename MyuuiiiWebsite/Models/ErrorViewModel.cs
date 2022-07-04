61fdf12717bf62874a630523cf0cae10494518e0
Myuuiii <mute@mutedevs.nl>
2022-07-04 13:57:18 +0200

    FUNKY

---
+++ MyuuiiiWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MyuuiiiWebsite.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
